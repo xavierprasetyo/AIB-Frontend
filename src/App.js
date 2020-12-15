cc6ad32556d0a1ef42d6931fa7efd19c87f18161
xavierprasetyo <xavierprasetyo89@gmail.com>
2020-12-15 19:05:16 +0700

    removing extra deps

--- src/App.js
+++ src/App.js
@@ -2,26 +2,6 @@ import './App.css';
 import { Navbar } from './components'
 import { MainPage } from './pages'
 
-const req = {
-  marriage: 2,
-  sex: 1,
-  edu: 4,
-  limit: 40,
-  pay_amt: {
-    sept: 13,
-    oct: 131
-  },
-  bil : {
-    sept: 13,
-    oct: 131
-  },
-  pay_status : {
-    sept: 13,
-    oct: 131
-  }
-}
-
-
 const App = () => {
   return (
     <div>
